7a47c360ece1f9b5a9cbafe093df1615e2fe98d4
Anilam3328 <melody3328@qq.com>
2025-12-07 01:53:37 +0800

    基础stopui

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseManager : MonoBehaviour
+
+{
+    public GameObject pausePanel;  
+    public bool isPaused = false;  
+
+    void Update()
+    {
+    
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused; 
+
+        if (isPaused)
+        {
+            Time.timeScale = 0; 
+            pausePanel.SetActive(true); 
+        }
+        else
+        {
+            Time.timeScale = 1; 
+            pausePanel.SetActive(false); 
+        }
+    }
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1; 
+        SceneManager.LoadScene("Opening"); 
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resume button clicked");
+        TogglePause();
+    }
+
+}
